150fb1e5bbc221a1c0359b9f78fbbb5463d72cd7
alisson0101 <alisson.monteiro@escolar.ifrn.edu.br>
2023-05-11 15:04:35 -0300

    atualizado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Empresas</title>
+    <title>Bem Vindo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/home-estilo.css') }}">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/"><h1>Sua Empresa</h1></a>
+    <a class="navbar-brand" href="/"><h1>Sua Empresa <img src="https://cdn-icons-png.flaticon.com/512/1599/1599904.png" alt="" id="incon"></h1 ></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
